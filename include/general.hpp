e8ed0f23fd7c4c50c1d93bdfb11de640d758716f
Vladislav Korotianets <vkorotanec@gmail.com>
2025-07-19 00:01:22 +0200

    Added faces constants to `general.hpp`.

--- include/general.hpp
+++ include/general.hpp
@@ -1,12 +1,28 @@
 #include <glm/fwd.hpp>
 #include <glm/vec3.hpp>
 #include <iostream>
+#include <vector>
 
 #define CODE_ERROR 1
 #define CODE_NO_ERROR 0
 #define CODE_FATAL_ERROR -1
 
+const std::vector<int> FRONT = {-1, 1, -1, 1, -1, -1, -1, -1, -1, 1, 1, -1, 1, -1, -1, -1, 1, -1};
+const std::vector<int> LEFT = {
+    1, 1, -1, 1, -1, 1, 1, -1, -1, 1, 1, 1, 1, -1, 1, 1, 1, -1,
+};
+const std::vector<int> BACK = {1, 1, 1, -1, -1, 1, 1, -1, 1, -1, 1, 1, -1, -1, 1, 1, 1, 1};
+const std::vector<int> RIGHT = {
+    -1, 1, 1, -1, -1, -1, -1, -1, 1, -1, 1, -1, -1, -1, -1, -1, 1, 1,
+};
+const std::vector<int> UP = {
+    -1, 1, 1, 1, 1, -1, -1, 1, -1, 1, 1, 1, 1, 1, -1, -1, 1, 1,
+};
+const std::vector<int> DOWN = {
+    -1, -1, -1, 1, -1, 1, -1, -1, 1, 1, -1, -1, 1, -1, 1, -1, -1, -1,
+};
+
 void printVector(glm::vec3 vec)
 {
-    std::cout << "X: " << vec.x << ", Y: " << vec.y << ", Z:" << vec.z; 
+    std::cout << "X: " << vec.x << ", Y: " << vec.y << ", Z:" << vec.z;
 }
