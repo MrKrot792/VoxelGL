9df73edb483ced31b263ad00ca6572e9bc9f54f5
Vladislav Korotianets <vkorotanec@gmail.com>
2025-07-19 13:36:08 +0200

    Added `Directions` struct that contains all directions of the cube

--- include/general.hpp
+++ include/general.hpp
@@ -1,23 +1,33 @@
+#pragma once
+
 #include <glm/fwd.hpp>
 #include <glm/vec3.hpp>
 #include <iostream>
 #include <vector>
 
-enum class RESULT_CODE {CODE_NO_ERROR, CODE_ERROR, CODE_FATAL_ERROR};
+enum class RESULT_CODE
+{
+    CODE_NO_ERROR,
+    CODE_ERROR,
+    CODE_FATAL_ERROR
+};
 
-const std::vector<int> FRONT = {-1, 1, -1, 1, -1, -1, -1, -1, -1, 1, 1, -1, 1, -1, -1, -1, 1, -1};
-const std::vector<int> LEFT = {
-    1, 1, -1, 1, -1, 1, 1, -1, -1, 1, 1, 1, 1, -1, 1, 1, 1, -1,
-};
-const std::vector<int> BACK = {1, 1, 1, -1, -1, 1, 1, -1, 1, -1, 1, 1, -1, -1, 1, 1, 1, 1};
-const std::vector<int> RIGHT = {
-    -1, 1, 1, -1, -1, -1, -1, -1, 1, -1, 1, -1, -1, -1, -1, -1, 1, 1,
-};
-const std::vector<int> UP = {
-    -1, 1, 1, 1, 1, -1, -1, 1, -1, 1, 1, 1, 1, 1, -1, -1, 1, 1,
-};
-const std::vector<int> DOWN = {
-    -1, -1, -1, 1, -1, 1, -1, -1, 1, 1, -1, -1, 1, -1, 1, -1, -1, -1,
+struct Directions
+{
+    static inline const std::vector<int> FRONT = {-1, 1, -1, 1, -1, -1, -1, -1, -1, 1, 1, -1, 1, -1, -1, -1, 1, -1};
+    static inline const std::vector<int> LEFT = {
+        1, 1, -1, 1, -1, 1, 1, -1, -1, 1, 1, 1, 1, -1, 1, 1, 1, -1,
+    };
+    static inline const std::vector<int> BACK = {1, 1, 1, -1, -1, 1, 1, -1, 1, -1, 1, 1, -1, -1, 1, 1, 1, 1};
+    static inline const std::vector<int> RIGHT = {
+        -1, 1, 1, -1, -1, -1, -1, -1, 1, -1, 1, -1, -1, -1, -1, -1, 1, 1,
+    };
+    static inline const std::vector<int> UP = {
+        -1, 1, 1, 1, 1, -1, -1, 1, -1, 1, 1, 1, 1, 1, -1, -1, 1, 1,
+    };
+    static inline const std::vector<int> DOWN = {
+        -1, -1, -1, 1, -1, 1, -1, -1, 1, 1, -1, -1, 1, -1, 1, -1, -1, -1,
+    };
 };
 
 void printVector(glm::vec3 vec)
