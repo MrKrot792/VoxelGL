8d6cdb8f496f685f39f8fe875403914e8301ad0a
Vladislav Korotianets <vkorotanec@gmail.com>
2025-08-05 21:15:44 +0200

    Added different generation technics
    Will be removed later

--- include/generation.hpp
+++ include/generation.hpp
@@ -1,27 +1,40 @@
 #pragma once
 
-#include "block.hpp"
 #include "FastNoiseLite.hpp"
+#include "block.hpp"
 
 #include <cmath>
 
 #include <glm/vec3.hpp>
 
-
 namespace generation
 {
+enum GENERATION_ALGORITHM
+{
+    SIMPLEX,
+    CELLULAR,
+};
+
+constexpr GENERATION_ALGORITHM currentAlgorithm = SIMPLEX;
+
 inline Block genBlockAt(glm::ivec3 at)
 {
     // INEFFICIENT
     FastNoiseLite noise;
-    noise.SetNoiseType(FastNoiseLite::NoiseType::NoiseType_OpenSimplex2);
+    if (currentAlgorithm == SIMPLEX)
+    {
+        noise.SetNoiseType(FastNoiseLite::NoiseType::NoiseType_OpenSimplex2);
+    }
+    else if (currentAlgorithm == CELLULAR) 
+    {
+    
+    }
 
     Block result = BlockTypes::getBlockById(BLOCK::DIRT);
 
-    if(at.y >= pow(noise.GetNoise((float)at.x, (float)at.z) * 4.f, 2))
+    if (at.y >= pow(noise.GetNoise((float)at.x, (float)at.z) * 4.f, 2))
         result = BlockTypes::getBlockById(BLOCK::AIR);
 
     return result;
 }
 } // namespace generation
-
