87ca7131efb34d6db461b0201a356fd4ee0618cd
Vladislav Korotianets <vkorotanec@gmail.com>
2025-08-05 21:16:53 +0200

    New generation technic: Cellular!

--- include/generation.hpp
+++ include/generation.hpp
@@ -25,14 +25,26 @@ inline Block genBlockAt(glm::ivec3 at)
     {
         noise.SetNoiseType(FastNoiseLite::NoiseType::NoiseType_OpenSimplex2);
     }
-    else if (currentAlgorithm == CELLULAR) 
+    else if (currentAlgorithm == CELLULAR)
     {
-    
+        noise.SetNoiseType(FastNoiseLite::NoiseType::NoiseType_Cellular);
+
+        noise.SetFrequency(0.015);
+        noise.SetFractalType(FastNoiseLite::FractalType::FractalType_FBm);
+        noise.SetFractalOctaves(2);
+        noise.SetFractalLacunarity(2.350);
+        noise.SetFractalGain(0.650);
+        noise.SetFractalWeightedStrength(0.390);
+
+        noise.SetCellularDistanceFunction(
+            FastNoiseLite::CellularDistanceFunction::CellularDistanceFunction_EuclideanSq);
+        noise.SetCellularReturnType(FastNoiseLite::CellularReturnType::CellularReturnType_Distance);
+        noise.SetCellularJitter(1.710);
     }
 
     Block result = BlockTypes::getBlockById(BLOCK::DIRT);
 
-    if (at.y >= pow(noise.GetNoise((float)at.x, (float)at.z) * 4.f, 2))
+    if (at.y >= pow(noise.GetNoise((float)at.x, (float)at.z) * 2.f, 6))
         result = BlockTypes::getBlockById(BLOCK::AIR);
 
     return result;
