c8faa91f1a5f6fab747415109e6973894d77414d
Vladislav Korotianets <vkorotanec@gmail.com>
2025-08-12 13:36:12 +0200

    FPSCounter source file

--- src/fps_count.cpp
+++ src/fps_count.cpp
@@ -1,28 +1,34 @@
 #include "fps_count.hpp"
 #include <GLFW/glfw3.h>
 
+double FPSCounter::start = 0.0;
+double FPSCounter::end = 0.0;
+double FPSCounter::seconds_count = 0.0;
+uint32_t FPSCounter::frame_count = 0;
+uint32_t FPSCounter::old_frame_count = 0;
+
 void FPSCounter::Start()
 {
-    this->start = glfwGetTime();   
+    start = glfwGetTime();   
 }
 
 void FPSCounter::End()
 {
-    this->end = glfwGetTime();   
+    end = glfwGetTime();   
 }
 
 uint32_t FPSCounter::GetFPS()
 {
     if(seconds_count >= 1.0)
     {
-        this->seconds_count = 0;
-        this->old_frame_count = this->frame_count;
-        this->frame_count = 0;
+        seconds_count = 0;
+        old_frame_count = frame_count;
+        frame_count = 0;
 
         return old_frame_count;
     }
 
-    seconds_count += GetDelta();
+    seconds_count += FPSCounter::GetDelta();
     frame_count++;
 
     return old_frame_count;
@@ -30,12 +36,12 @@ uint32_t FPSCounter::GetFPS()
 
 double FPSCounter::GetMediumFPS()
 {
-    double difference = this->GetDelta();
+    double difference = FPSCounter::GetDelta();
     return 1.0 / difference;
 }
 
 double FPSCounter::GetDelta()
 {
-    double difference = this->end - this->start;
+    double difference = end - start;
     return difference;
 }
